309d47bba60b5269a3c7ae256839b38be6f28ab4
Jihee Nam <jinam@c8r1s14.42seoul.kr>
2022-05-21 14:41:27 +0900

    git git

--- rush00.c
+++ rush00.c
@@ -6,8 +6,23 @@
 /*   By: jinam <jinam@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 11:29:50 by jinam             #+#    #+#             */
-/*   Updated: 2022/05/21 11:29:52 by jinam            ###   ########.fr       */
+/*   Updated: 2022/05/21 14:37:06 by jinam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void rush(int a, int b);
+void ft_putchar(char ch);
 
+void rush(int a, int b)
+{
+	int	first_cycle;
+
+	first_cycle = b;
+
+	while (first_cycle > 0)
+	{
+		//ft_putchar(a, b, first_cycle);
+		ft_putchar('a');
+		first_cycle --;
+	}
+}
