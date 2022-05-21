ed3c42c24fdbd8df43f0b360961136a1a3a3b5c6
Junsang Mun <jumun@c8r1s13.42seoul.kr>
2022-05-21 15:33:58 +0900

    norm... I really hate it

--- rush00.c
+++ rush00.c
@@ -6,34 +6,33 @@
 /*   By: jinam <jinam@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 11:29:50 by jinam             #+#    #+#             */
-/*   Updated: 2022/05/21 15:17:03 by jumun            ###   ########.fr       */
+/*   Updated: 2022/05/21 15:33:36 by jumun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 
-void rush(int a, int b);
-void ft_putchar(char ch);
-void rush00(int first,int second, int third);
+void	rush(int a, int b);
+void	ft_putchar(char ch);
+void	rush00(int first, int second, int third);
 
-void hamsoo(char a, char b, int first, int count)
+void	printout(char a, char b, int first, int count)
 {
-			if (count==1 || count == first)
-				ft_putchar(a);
-			else
-				ft_putchar(b);
+	if (count == 1 || count == first)
+		ft_putchar(a);
+	else
+		ft_putchar(b);
 }
 
-void rush00(int first, int second, int third)
+void	rush00(int first, int second, int third)
 {
 	int	count;
 
 	count = first;
-
 	if (third == 1 || third == second)
 	{
-		while (count > 0)	
+		while (count > 0)
 		{
-			hamsoo('o', '-', first, count);
+			printout('o', '-', first, count);
 			count --;
 		}
 	}
@@ -41,24 +40,21 @@ void rush00(int first, int second, int third)
 	{
 		while (count > 0)
 		{
-			hamsoo('|', ' ', first, count);
+			printout('|', ' ', first, count);
 			count --;
 		}
 	}
 	ft_putchar('\n');
 }
 
-void rush(int a, int b)
+void	rush(int a, int b)
 {
 	int	first_cycle;
 
 	first_cycle = b;
-
 	while (first_cycle > 0)
 	{
-		//printf("%d %d %d\n",a,b,first_cycle);
-		//ft_putchar(a, b, first_cycle);
-		rush00(a,b,first_cycle);
+		rush00(a, b, first_cycle);
 		first_cycle --;
 	}
 }
