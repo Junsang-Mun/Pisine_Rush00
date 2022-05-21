3c9c2fea78e6383a8e2a001d6ef849e0ed1f6f38
Minseung Kang <minseuka@c8r1s12.42seoul.kr>
2022-05-21 20:36:43 +0900

    rush03.c

---
+++ rush03.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinam <jinam@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/21 11:29:50 by jinam             #+#    #+#             */
+<<<<<<< HEAD:rush00.c
+/*   Updated: 2022/05/21 20:14:07 by jinam            ###   ########.fr       */
+=======
+/*   Updated: 2022/05/21 20:26:48 by minseuka         ###   ########.fr       */
+>>>>>>> minseuka:rush03.c
+/*                                                                            */
+/* ************************************************************************** */
+
+void	rush(int col, int row);
+void	ft_putchar(char ch);
+void	rush03(int col, int row, int index_row);
+
+void	printout(char edge, char wall, int col, int index_col)
+{
+	if (index_col == 1 || index_col == col)
+		ft_putchar(edge);
+	else
+		ft_putchar(wall);
+}
+
+void	end_print(int col, int row, int index_col)
+{
+	if (col == 1 && row != 1)
+		printout('A', '\0', col, index_col);
+	else if (col == 1 && row == 1)
+		printout('A', '\0', col, index_col);
+	else
+		printout('C', '\0', col, index_col);
+}
+
+void	rush03(int col, int row, int index_row)
+{
+	int	index_col;
+
+	index_col = col;
+	if (index_row == 1 || index_row == row)
+	{
+		while (index_col > 1)
+		{
+			printout('A', 'B', col, index_col);
+			index_col --;
+		}
+		end_print(col, row, index_col);
+	}
+	else
+	{
+		while (index_col > 0)
+		{
+			printout('B', ' ', col, index_col);
+			index_col --;
+		}
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int col, int row)
+{
+	int	index_row;
+
+	index_row = row;
+	while (index_row > 0)
+	{
+		rush03(col, row, index_row);
+		index_row --;
+	}
+}
