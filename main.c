6bdf403af8505aa00dc3c345a06394245922dc05
Minseung Kang <minseuka@c8r1s13.42seoul.kr>
2022-05-22 14:53:54 +0900

    main.c : add argv function

--- main.c
+++ main.c
@@ -6,13 +6,34 @@
 /*   By: jinam <jinam@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 11:29:08 by jinam             #+#    #+#             */
-/*   Updated: 2022/05/22 11:23:08 by jinam            ###   ########.fr       */
+/*   Updated: 2022/05/22 14:42:11 by minseuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-void	rush(int a, int b);
 
-int	main(void)
+void	rush(int x, int y);
+
+int	strtoint(char *str)
 {
-	rush (4, 4);
+	int	out;
+	int	index;
+	int	outbefore;
+
+	index = 0;
+	outbefore = 0;
+	out = 0;
+	while (47 < str[index] && str[index] < 58)
+	{
+		out = outbefore * 10 + (str[index] - 48);
+		outbefore = out;
+		index ++;
+	}
+	return (out);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 3)
+		return (0);
+	rush(strtoint(argv[1]), strtoint(argv[2]));
 	return (0);
 }
